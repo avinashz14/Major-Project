06e381dc5a6b5d432e217886a8a4735f6f2da149
nithost <41814672+avinashz14@users.noreply.github.com>
2021-04-28 19:07:39 +0530

    Update Experiments.m

--- Experiments.m
+++ Experiments.m
@@ -1,10 +1,3 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-%   Alp Sayin - alp_sayin[at]ieee[dot]org
-%   Matlab Genetic Algorithm
-%   Spring 2012
-% 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear
 close all
